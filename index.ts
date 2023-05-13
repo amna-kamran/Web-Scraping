85b5df12be5f66b730c294baf7b49ebfcfa3b769
jurazil <jurazilgreen@gmail.com>
2023-05-13 17:07:49 +0500

    data fetching?

--- index.ts
+++ index.ts
@@ -10,9 +10,18 @@ const main = async()=>{
 
     //evaluating the page gives us a document
     const bookData = await page.evaluate(()=>{
-        
+        const bookPods =  Array.from(document.querySelectorAll('.product_pod'))
+        //what data do we want from each book pod?
+        //using map method to apply a function that extracts the required data from each pod
+        //the function that we will be mapping the book pods to will take
+        //an input of book with type any
+        const data = bookPods.map((book:any)=>({
+            title: book.querySelector('h3 a').getAttributes('title'),
+            price: book.querySelector('.price_color').innerText,
+            
+        }))
     })
+    console.log(bookData)
     }
-}
 
 main()
