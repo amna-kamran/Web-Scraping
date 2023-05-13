43b4f68802fb206b8f5ac561a4a5507e679a9f64
jurazil <jurazilgreen@gmail.com>
2023-05-13 16:41:21 +0500

    setup

--- index.ts
+++ index.ts
@@ -1,1 +1,12 @@
 const puppeteer = require('puppeteer')
+import { Browser } from "puppeteer"
+
+const url = 'https://books.toscrape.com/'
+
+const main = async()=>{
+    const browser : Browser = await puppeteer.launch({headless:false})
+    const page = await browser.newPage()
+    await page.goto(url)
+}
+
+main()
